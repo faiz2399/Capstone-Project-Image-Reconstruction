85adf4fe732ada71eea8a8d3d7003839e34794e5
faiz2399 <khwajafaiz06@gmail.com>
2023-01-16 16:39:40 -0500

    FK editing main file to show multiple plots

--- Faiz/main_faiz.py
+++ Faiz/main_faiz.py
@@ -1,1 +1,31 @@
-print('Faiz')
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+
+file = open('imagenet_subset.pkl', 'rb')
+
+# dump information to that file
+ds_p = pickle.load(file)
+
+
+
+plt.figure()
+plt.imshow(ds_p[1])
+
+plt.figure()
+
+plt.imshow(np.array(ds_p['train'][1]["image"]))
+
+
+plt.figure()
+
+plt.imshow(np.array(ds_p['train'][4]["image"]))
+
+
+
+# close the file
+file.close()
